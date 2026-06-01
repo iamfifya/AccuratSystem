22ce263121c5d55f0b3cc2d5d9069ad7467c334a
iamfifya <dimakuraedov@gmail.com>
2026-06-01 11:22:53 +0300

    Update TenantFeature.cs

--- AccuratSystem.Contracts/Models/TenantFeature.cs
+++ AccuratSystem.Contracts/Models/TenantFeature.cs
@@ -10,7 +10,7 @@ namespace AccuratSystem.Contracts.Models
         public int CompanyId { get; set; }
 
         public bool IsUpsellEnabled { get; set; } = false; // Модуль Умный кассир, который предлагает дополнительные услуги при продаже
-        public bool IsServicesEnabled { get; set; } = false; // Модуль Управления услугами, который позволяет создавать и управлять услугами, которые предоставляет компания
+        public bool IsServicesEnabled { get; set; } = true; // Модуль Управления услугами, который позволяет создавать и управлять услугами, которые предоставляет компания
         public bool IsCrmMarketingEnabled { get; set; } = false; // Модуль CRM и маркетинга, который позволяет управлять клиентами, создавать маркетинговые кампании и анализировать их эффективность
         public bool IsTelegramBossEnabled { get; set; } = false; // Модуль Telegram Босс, который позволяет получать уведомления о продажах и других событиях в Telegram
         public bool IsReputationEnabled { get; set; } = false; // Модуль Репутация, который позволяет управлять отзывами клиентов и анализировать их для улучшения качества обслуживания
