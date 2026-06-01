cd41be9de57a956cbf4698c54059919518e5e6f4
iamfifya <dimakuraedov@gmail.com>
2026-06-01 10:53:12 +0300

    Придумал новый модуль IsReputationEnabled
    IsReputationEnabled (DLC): Вызов краткой и развернутой сводки из сервисов для отзывов(Яндекс, Гугл, 2ГИС).

--- AccuratSystem.Contracts/Models/TenantFeature.cs
+++ AccuratSystem.Contracts/Models/TenantFeature.cs
@@ -13,6 +13,7 @@ namespace AccuratSystem.Contracts.Models
         public bool IsStorageEnabled { get; set; } = false;
         public bool IsCrmMarketingEnabled { get; set; } = false;
         public bool IsTelegramBossEnabled { get; set; } = false;
+        public bool IsReputationEnabled { get; set; } = false;
 
         // ДОБАВИЛИ НАВИГАЦИОННОЕ СВОЙСТВО, чтобы работал Include() в контроллере
         public Company Company { get; set; }
---
+++ Akkurat.WebAPI/Migrations/20260601075204_NewDLCIsReputationEnabled.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accurat.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewDLCIsReputationEnabled : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReputationEnabled",
+                table: "TenantFeatures",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "TenantFeatures",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsReputationEnabled",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "TenantFeatures",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsReputationEnabled",
+                value: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReputationEnabled",
+                table: "TenantFeatures");
+        }
+    }
+}
